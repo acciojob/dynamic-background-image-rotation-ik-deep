5eab07a8836fd15c0b1c3b6e1e8393a49c27c046
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2024-01-13 08:36:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+ function setBodyBackground() {
+            const body = document.body;
+            const portraitImage = "https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg";
+            const landscapeImage = "https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg";
+
+            if (window.innerHeight > window.innerWidth) {
+                // Portrait mode
+                body.style.backgroundImage = `url('${portraitImage}')`;
+            } else {
+                // Landscape mode
+                body.style.backgroundImage = `url('${landscapeImage}')`;
+            }
+        }
+
+        // Initial call to set background on page load
+        setBodyBackground();
+  window.addEventListener('orientationchange', setBodyBackground);
+        window.addEventListener('resize', setBodyBackground);
